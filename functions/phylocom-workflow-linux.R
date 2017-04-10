97751fb5af46d1854ca52255d519f8fef02209dd
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2017-04-10 14:05:20 -0300

    #3 rotina linux implementada

--- functions/phylocom-workflow-linux.R
+++ functions/phylocom-workflow-linux.R
@@ -6,40 +6,37 @@
 # https://greggilbertlab.sites.ucsc.edu/wp-content/uploads/sites/276/2015/10/R_Class10b_PicatePhylomatic.pdf
 
 temp <- tempfile()
-download.file("http://phylodiversity.net/phylocom/phylocom-4.2.zip", temp, method = "wget")
+download.file("https://github.com/phylocom/phylocom/releases/download/4.2/phylocom-4.2.zip", temp, method = "wget")
 unzip(temp)
 
-# setwd("phylocom-4.2/w32")
-# getwd()
-# 
-# write.tree(get.tree.phylomatic, "tree-phylomatic")
-#
-# file.rename(from = "../example_data/bladj_example/wikstrom.ages",
-#            to = "ages")
-# 
-# dated.tree <- system("phylocom bladj -f tree-phylomatic > dated-tree",
-#                      intern = TRUE)
-# 
-# write(dated.tree, "dated-tree")
-# 
-# dated.clean <- scan("dated-tree", what = "character", nmax = -1, sep = "]")
-# 
-# dated.clean <-
-#   sub(pattern = ")euphyllophyte:1.000000",
-#       replacement = "",
-#       x = dated.clean)
-# 
-# dated.clean <- sub(pattern = "\\(",
-#                    replacement = "",
-#                    x = dated.clean)
-# 
-# dated.clean <- gsub(pattern = " ",
-#                     replacement = "",
-#                     x = dated.clean)
-# 
-# dated.clean <- read.newick(text = dated.clean)
-# 
-# setwd("../../")
-# getwd()
-# 
-# write.tree(dated.clean, "data/dated.clean.txt")
+setwd("phylocom-4.2/src")
+system("make", invisible = FALSE)
+
+write.tree(get.tree.phylomatic, "tree-phylomatic")
+
+file.copy(from = "../example_data/bladj_example/wikstrom.ages", to = ".")
+
+file.rename(from = "wikstrom.ages", to = "ages")
+
+dated.tree <- system("./phylocom bladj -f tree-phylomatic > dated-tree")
+
+dated.clean <- scan("dated-tree", what = "character", nmax = -1, sep = "]")
+ 
+dated.clean <-
+  sub(pattern = ")euphyllophyte:1.000000",
+      replacement = "",
+      x = dated.clean)
+ 
+dated.clean <- sub(pattern = "\\(",
+                   replacement = "",
+                   x = dated.clean)
+ 
+dated.clean <- gsub(pattern = " ",
+                    replacement = "",
+                    x = dated.clean)
+ 
+dated.clean <- read.newick(text = dated.clean)
+ 
+setwd("../../")
+
+write.tree(dated.clean, "data/dated.clean.txt")
